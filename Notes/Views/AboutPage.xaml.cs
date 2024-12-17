0ec19755e7bb3ab5b9e3b0ee101b068d38a95494
byPronox <bypronoxyt@gmail.com>
2024-12-17 10:58:51 -0500

    MVVM NOTES

--- Notes/Views/AboutPage.xaml.cs
+++ Notes/Views/AboutPage.xaml.cs
@@ -1,5 +1,3 @@
-using Notes.Models;
-
 namespace Notes.Views;
 
 public partial class AboutPage : ContentPage
@@ -7,23 +5,5 @@ public partial class AboutPage : ContentPage
     public AboutPage()
     {
         InitializeComponent();
-
-        var about = new About
-        {
-            Icon = "chill.jpg"  // Usamos la foto chill.jpg
-        };
-
-        BindingContext = about;
     }
-
-    private async void LearnMore_Clicked(object sender, EventArgs e)
-    {
-        if (BindingContext is Models.About about)
-        {
-            // Navigate to the specified URL in the system browser.
-            await Launcher.Default.OpenAsync(about.MoreInfoUrl);
-        }
-    }
-
-    
 }
